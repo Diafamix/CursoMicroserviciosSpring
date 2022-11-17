471a2cdde54168dfdadf9b3c298c6ad179d3ceef
OPTIMISSA\carlos.cueva <carlos.cueva@optimissa.com>
2022-11-17 10:35:38 +0100

    Creacion del microservicio-eureka

---
+++ microservicio-eureka/src/test/java/com/microservicio/microservicioeureka/MicroservicioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.microservicioeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
